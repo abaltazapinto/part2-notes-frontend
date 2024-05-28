5107214cab5b522a31fb089704cc00c7f87719d5
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-28 20:26:59 +0100

    .....

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './src/index.html', // Ensure this is pointing to your index.html file in the src directory
-      onwarn(warning, warn) {
-        if (warning.code === 'THIS_IS_UNDEFINED' || warning.code === 'UNRESOLVED_IMPORT') {
-          return;
-        }
-        warn(warning);
-      },
-    },
-  },
   server: {
     proxy: {
       '/api': {
