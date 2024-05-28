a69d0fcbc8d502a496c4c48306d1620425455101
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-28 20:24:12 +0100

    ""

--- src/services/notes.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
-export default { getAll, create, update }
