2e7a6b53402307fbdb30fb3656e74ed469d3e286
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-28 19:34:57 +0100

    done?

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  build: {
-    outDir: '../dist',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-});
+})
