5b81d4e4430269d4b1707af400e54b5ad6bd60d3
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-28 20:10:25 +0100

    dsad

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn (warning, warn) {
-        if (warning.code === 'THIS_IS_UNDEFINED' || warning.code === 'UNRESOLVED_IMPORT') {
-          return;
-        }
-        warn(warning);
-      }
-    }
-  },
   server: {
     proxy: {
       '/api': {
