6c0f84eef8f8b04ed10714fa82d399b7d73b5aca
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-30 09:44:10 +0100

    server

---
+++ index.js
@@ -0,0 +1,23 @@
+import cors from 'cors';
+import http from 'http';
+
+const app  = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+});
+
+// handles any requests that dont match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const cors = require('cors')
-app.use(cors())
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
