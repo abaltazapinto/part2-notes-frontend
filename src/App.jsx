670c6e1a8b58527fc874de32ecf55320dc7a6c4d
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-30 15:39:45 +0100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [showImportant, setShowImportant] = useState(false);
   const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001/api/notes'; 
  
   useEffect(() => {
@@ -11,12 +12,36 @@ function App() {
       .then(data => setNotes(data))
   }, [apiUrl])
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    fetch(`${apiUrl}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(changedNote)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setNotes(notes.map(note => note.id !== id ? note : data))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowImportant(!showImportant)}>
+        {showImportant ? 'Show All' : 'Show Important'}
+      </button>
       <ul>
         {notes.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance = {() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
     </div>
