16f1e256b2b60686ddc58198bc96144ba1cb4514
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-30 09:49:36 +0100

    ....

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import { fileURLToPath } from 'url';
 
 const app  = express();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
