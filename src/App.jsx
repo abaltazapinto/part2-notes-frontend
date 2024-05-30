d45e4ddb78ba2f789512d60f08746db35d64fcbb
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-30 17:44:44 +0100

    ldsad

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Note from './components/Note';
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [showImportant, setShowImportant] = useState(false);
+  const [newNote, setNewNote] = useState('');
   const apiUrl = 'http://localhost:3001/api/notes';
 
   useEffect(() => {
@@ -44,24 +45,69 @@ const App = () => {
       .catch(error => console.error('Update error:', error));
   };
 
+  const addNote = (event) => {
+    event.prevenDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    };
+    
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    body: JSON.stringify(noteObject)
+  })
+    .then(response => response.json())
+    .then(data => {
+      setNotes(notes.concat(data));
+      setNewNote('');
+    })
+    .catch(error => console.error('Add note error:', error));
+  };
+
+  //app para deleatar notas
+  const deleteNote = (id) => {
+    const url = `${apiUrl}/${id}`;
+
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        setNotes(notes.filter(note => note.id !== id));
+      })
+      .catch(error => console.error('Delete error:', error));
+  };
+
   const notesToShow = showImportant ? notes.filter(note => note.important) : notes;
 
   return (
+    <>
     <div>
       <h1>Notes</h1>
       <button onClick={() => setShowImportant(!showImportant)}>
         {showImportant ? 'Show All' : 'Show Important'}
       </button>
       <ul>
-        {notesToShow.map(note => 
-          <Note 
-            key={note.id} 
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)} 
-          />
+        {notesToShow.map(note => <Note
+          key={note.id}
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
+          deleteNote={() => deleteNote(note.id)} />
         )}
       </ul>
-    </div>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)} />
+        <button type="submit">Save</button>
+      </form>
+      </div>
+      </>
   );
 }
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,15 @@
-const Note = ({ note }) => {
+import React from "react"
+
+const Note = ({ note, toggleImportance, deleteNote }) => {
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>
+        {note.important ? 'make not important' : 'make important'}
+      </button>
+      <button onClick={deleteNote}>delete</button> 
+    </li>
+
   )
 }
 
